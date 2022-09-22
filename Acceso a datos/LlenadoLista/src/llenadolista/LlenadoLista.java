6feac2351f167dd1f915a511b3a39d2e072e9f56
galve <galvez.jangofett@gmail.com>
2022-09-22 17:54:07 +0200

    Initial Commit

---
+++ Acceso a datos/LlenadoLista/src/llenadolista/LlenadoLista.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package llenadolista;
+
+import llenadolista.Controller.Lista;
+
+/**
+ *
+ * @author galve
+ */
+public class LlenadoLista {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        lista.setVisible(true);
+    }
+    
+}
